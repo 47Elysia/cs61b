6f13c27a5f1a7676c622f3a31073bf9559b79b66
zhupin <zp@zju.com>
2022-09-24 15:37:34 +0800

    proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -5,26 +5,45 @@ public class TestArrayDequeGold {
     public void testaddFirst() {
         StudentArrayDeque<Integer> adq = new StudentArrayDeque<Integer>();
         ArrayDequeSolution<Integer> bdq = new ArrayDequeSolution<Integer>();
+        String log = new String();
 
-
-        for (int i = 0; i < 10; i += 1) {
-            double randnumber = StdRandom.uniform();
-
-            if (randnumber > 0.5) {
-                adq.addFirst(i);
-                bdq.addFirst(i);
+        for (int i = 0; i < 1000; i += 1) {
+            if (adq.size() == 0) {
+                Integer x = StdRandom.uniform(2);
+                Integer randnumber = StdRandom.uniform(1000);
+                if (x == 0) {
+                    adq.addFirst(randnumber);
+                    bdq.addFirst(randnumber);
+                    log += "addFirst: " + randnumber + "\n";
+                } else {
+                    adq.addLast(randnumber);
+                    bdq.addLast(randnumber);
+                    log += "addLast: " + randnumber + "\n";
+                }
             } else {
-                adq.addLast(i);
-                bdq.addLast(i);
+                Integer x = StdRandom.uniform(4);
+                Integer randnumber = StdRandom.uniform(1000);
+                Integer aremove = 1;
+                Integer bremove = 1;
+                if (x == 0) {
+                    adq.addFirst(randnumber);
+                    bdq.addFirst(randnumber);
+                    log += "addFirst: " + randnumber +"\n";
+                } else if (x == 1) {
+                    adq.addLast(randnumber);
+                    bdq.addLast(randnumber);
+                    log += "addLast: " + randnumber + "\n";
+                } else if (x == 2) {
+                    aremove = adq.removeFirst();
+                    bremove = bdq.removeFirst();
+                    log += "removeFirst: " + randnumber + "\n";
+                } else {
+                    aremove = adq.removeLast();
+                    bremove = bdq.removeLast();
+                    log += "removeLast: " + randnumber + "\n";
+                }
+                assertEquals(log, bremove, aremove);
             }
-        }
-
-        for (int i = 0; i < 10; i += 1) {
-            Integer expect = adq.removeLast();
-            Integer actual = bdq.removeLast();
-            assertEquals("Oh noooo!\nThis is bad:\n   Random number " + actual
-                            + " not equal to " + expect + "!",
-                    expect, actual);
         }
     }
 }
