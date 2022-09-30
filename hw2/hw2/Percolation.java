5b9510f2cfc60372e386f7df4ed626d6f1bbe9fb
zhupin <zp@zju.com>
2022-09-30 23:48:19 +0800

    hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,71 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.awt.*;
+import java.util.HashSet;
+
 public class Percolation {
+    private int numberofopensites = 0;
+    private boolean[][] grid;
+    private boolean[][] full;
+    private HashSet hashSet = new HashSet();
+    public Percolation(int N) throws IllegalArgumentException {
+       grid = new boolean[N][N];
+       full = new boolean[N][N];
+       hashSet.add(0);
+    }
+    public void open(int row, int col) throws IndexOutOfBoundsException {
+        if (grid[row][col] == false) {
+           grid[row][col] = true;
+        }
+    }
+    public boolean isOpen(int row, int col) throws IndexOutOfBoundsException {
+        numberofopensites += 1;
+        return grid[row][col];
+    }
+    public boolean isFull(int row, int col) throws IndexOutOfBoundsException {
+        if (!isOpen(row, col)) {
+            return false;
+        } else {
+            if (row == 0) {
+                full[row][col] = true;
+                return true;
+            }
+            if (isvalid(row + 1, col) && full[row + 1][col]) {
+                full[row][col] = true;
+                return true;
+            }
+            if (isvalid(row - 1, col) && full[row - 1][col]) {
+                full[row][col] = true;
+                return true;
+            }
+            if (isvalid(row, col + 1) && full[row][col + 1]) {
+                full[row][col] = true;
+                return true;
+            }
+            if (isvalid(row, col - 1) && full[row][col - 1]) {
+                full[row][col] = true;
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean isvalid(int row, int col) {
+        int height = grid.length;
+        return row >= 0 && row < height && col >= 0 && col < height;
+    }
+    public int numberOfOpenSites() {
+        return numberofopensites;
+    }
+    public boolean percolates() {
+        int height = grid.length - 1;
+        for (int col = 0; col < grid.length; col += 1) {
+            if (isFull(height, col)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,40 @@
 package hw2;
-
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 public class PercolationStats {
+    private double[] sample;
+    private double T;
+    public PercolationStats(int N, int T, PercolationFactory pf) throws IllegalArgumentException {
+        this.T = T;
+        sample = new double[N];
+        int x;
+        int y;
+        for (int i = 0; i < T; i += 1) {
+           Percolation percolation= pf.make(N);
+           while (!percolation.percolates()) {
+               do {
+                x = StdRandom.uniform(N);
+                y = StdRandom.uniform(N);
+               } while (percolation.isOpen(x, y));
+               percolation.open(x, y);
+           }
+        sample[i] = (double) percolation.numberOfOpenSites() / (N * N);
+        }
+    }
+    public double mean() {
+        return StdStats.mean(sample);
+    }
+    public double stddev() {
+        return StdStats.stddev(sample);
+    }
+    public double confidenceLow() {
+        return mean() - 1.96 * Math.sqrt(stddev()) / Math.sqrt(T);
+    }
+    public double confidenceHigh() {
+        return mean() + 1.96 * Math.sqrt(stddev()) / Math.sqrt(T);
+    }
+
+
+    
 
 }
