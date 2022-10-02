ccbaf979c89eb92eb9101f59fb8c07b519b2615d
zhupin <zp@zju.com>
2022-10-02 16:22:39 +0800

    lab9

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -53,19 +53,25 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        for (K k : buckets[hash(key)]) {
+           if (k == key) {
+               return buckets[hash(key)].get(key);
+           }
+        }
+        return null;
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        buckets[hash(key)].put(key, value);
+        size += 1;
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
