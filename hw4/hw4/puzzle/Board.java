db760758b1762d6c4caf37a5529881311437bd7a
zhupin <zp@zju.com>
2022-10-05 20:26:55 +0800

    hw4

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,10 +1,147 @@
 package hw4.puzzle;
+import edu.princeton.cs.algs4.Queue;
 
-public class Board {
+public class Board implements WorldState{
+    private int[][] tiles;
+    private int N;
+    private static int blank = 0;
+    private int estimatedist;
+    /**Constructs a board from an N-by-N array of tiles where
+     * tiles[i][j] = tile at row i, column j*/
+    public Board(int[][] tiles) {
+        if (tiles == null || tiles[0] == null || tiles.length != tiles[0].length) {
+            throw new IllegalArgumentException();
+        }
+        this.N = tiles.length;
+        this.tiles = new int[N][N];
+        estimatedist = -1;
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                this.tiles[i][j] = tiles[i][j];
+            }
+        }
+    }
 
+    /**
+     * Returns value of tile at row i, column j (or 0 if blank)*/
+    public int tileAt(int i, int j) {
+        validate(i, j);
+        return tiles[i][j];
+    }
+    private void validate(int i, int j) {
+        if (i >= N || j >= N || i < 0 || j < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    /*
+    Returns the board size N
+     */
+    public int size() {
+        return N;
+    }
+    /*
+    Returns the neighbors of the current board
+     */
+    @Override
+    public Iterable<WorldState> neighbors() {
+        Queue<WorldState> neighbor = new Queue<>();
+        int blankX = -1;
+        int blankY = -1;
+        for (int i = 0; i< N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                if (tiles[i][j] == blank) {
+                    blankX = i;
+                    blankY = j;
+                    break;
+                }
+            }
+        }
+        int[][] around = new int[N][N];
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                around[i][j] =tileAt(i, j);
+            }
+        }
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                if (Math.abs(blankX - i) + Math.abs(blankY - j) == 1) {
+                    around[blankX][blankY] = around[i][j];
+                    around[i][j] = blank;
+                    neighbor.enqueue(new Board(around));
+                    around[i][j] = around[blankX][blankY];
+                    around[blankX][blankY] = blank;
+                }
+            }
+        }
+        return neighbor;
+    }
+    /*
+    Hamming estimate described below
+     */
+    public int hamming() {
+        int distance = 0;
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                if (tiles[i][j] == 0) {
+                    continue;
+                }
+                if (tiles[i][j] != i * N + j + 1) {
+                    distance += 1;
+                }
+            }
+        }
+        return distance;
+    }
+    /*
+    Manhattan estimate described below
+     */
+    public int manhattan() {
+        int distance = 0;
+        for (int i = 0; i < N; i += 1) {
+            for (int j = 0; j < N; j += 1) {
+                if (tiles[i][j] == 0) {
+                    continue;
+                }
+                if (tiles[i][j] != i * N + j + 1) {
+                    int x = (tiles[i][j] - 1) / N;
+                    int y = (tiles[i][j] - 1) % N;
+                    distance += Math.abs(x - i) + Math.abs(y - j);
+                }
+            }
+        }
+        return distance;
+    }
+    /*
+     * Estimated distance to goal. This method should
+     * simply return the results of manhattan() when submitted to
+     * Gradescope.
+     */
+    public int estimatedDistanceToGoal() {
+        if (estimatedist == -1) {
+            estimatedist = manhattan();
+        }
+        return estimatedist;
+    }
+    /*
+     *Returns true if this board's tile values are the same
+     *position as y's
+     */
+    public boolean equals(Object y) {
+        if (y == this) {
+            return true;
+        }
+        if (y == null || y.getClass() != this.getClass()) {
+            return false;
+        }
+        Board newy= (Board) y;
+        if (newy.hamming() == this.hamming()) {
+            return true;
+        }
+        return false;
+    }
     /** Returns the string representation of the board. 
       * Uncomment this method. */
-    /*public String toString() {
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
@@ -16,6 +153,6 @@ public class Board {
         }
         s.append("\n");
         return s.toString();
-    }*/
+    }
 
 }
---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,87 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.ArrayList;
+
+public class Solver {
+    private class Searchnode implements Comparable<Searchnode> {
+        private WorldState state;
+        private int moves;
+        private Searchnode pre;
+        private Searchnode(WorldState state, int moves, Searchnode node) {
+            this.state = state;
+            this.moves = moves;
+            this.pre = node;
+        }
+        private WorldState getstate() {
+            return this.state;
+        }
+        private int getMoves() {
+            return this.moves;
+        }
+        private Searchnode getPre() {
+            return this.pre;
+        }
+        @Override
+        public int compareTo(Searchnode o) {
+            return this.moves + this.state.estimatedDistanceToGoal() - o.moves - o.state.estimatedDistanceToGoal();
+        }
+    }
+    private MinPQ<Searchnode> searchnode = new MinPQ<>();
+    private int tomoves;
+    private ArrayList solution;
+
+    private void returnanswer(Searchnode o) {
+        tomoves = o.moves;
+        solution = new ArrayList();
+        Searchnode goal = o;
+        while (goal != null) {
+            solution.add(goal.state);
+            goal = goal.pre;
+        }
+    }
+    /**
+     * Constructor which solves the puzzle, computing
+     * everything necessary for moves() and solution() to
+     * not have to solve the problem again. Solves the
+     * puzzle using the A* algorithm. Assumes a solution exists.
+     */
+    public Solver(WorldState initial) {
+        searchnode.insert(new Searchnode(initial, 0, null));
+        while (true) {
+            Searchnode min = searchnode.delMin();
+            if (min.state.isGoal()) {
+                returnanswer(min);
+                return;
+            } else {
+                for (WorldState neighbor : min.state.neighbors()) {
+                    if (min.pre == null || !min.pre.state.equals(neighbor)) {
+                        searchnode.insert(new Searchnode(neighbor, min.moves + 1, min));
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns the minimum number of moves to solve the puzzle starting
+     * at the initial WorldState.
+     */
+    public int moves() {
+        return tomoves;
+    }
+
+    /**
+     * Returns a sequence of WorldStates from the initial WorldState
+     * to the solution.
+     */
+    public Iterable<WorldState> solution() {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i <= tomoves; i += 1) {
+            list.add(solution.get(tomoves - i));
+        }
+        return list;
+    }
+
+}
